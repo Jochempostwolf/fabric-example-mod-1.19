896dacb414a2a1d04939a6d7c440f50557f475a5
Jochem <jochempostwolf@gmail.com>
2022-09-20 12:32:29 +0200

    Saved changes

---
+++ src/main/java/net/jochem/tutorialmod/item/ModItemGroup.java
@@ -0,0 +1,8 @@
+package net.jochem.tutorialmod.item;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+
+public class ModItemGroup {
+    public static final ItemGroup COOKIES = FabricItemGroupBuilder.build()
+}
