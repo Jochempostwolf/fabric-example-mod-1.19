3f255f3812ca01f579620107290c76f271cf665e
Jochem <jochempostwolf@gmail.com>
2022-09-20 19:15:48 +0200

    Added Edible Cookies and smelting recepies

--- src/main/java/net/jochem/tutorialmod/item/ModItems.java
+++ src/main/java/net/jochem/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.jochem.tutorialmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.jochem.tutorialmod.TutorialMod;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -10,6 +11,9 @@ public class ModItems {
 
     public static final Item COOKIE_DOUGH = registerItem("cookie_dough",
             new Item(new FabricItemSettings().group(ModItemGroup.COOKIES)));
+    public static final Item BAKED_COOKIE = registerItem("baked_cookie",
+            new Item(new FabricItemSettings().group(ModItemGroup.COOKIES)
+                    .food(new FoodComponent.Builder().snack().hunger(5).saturationModifier(5f).build())));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
