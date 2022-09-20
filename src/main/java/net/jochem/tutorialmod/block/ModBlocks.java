17164fe9e28e29e92ce56d70edbfa0b0ad3b86ac
Jochem <jochempostwolf@gmail.com>
2022-09-20 14:44:58 +0200

    Added Ores

--- src/main/java/net/jochem/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/jochem/tutorialmod/block/ModBlocks.java
@@ -6,16 +6,24 @@ import net.jochem.tutorialmod.TutorialMod;
 import net.jochem.tutorialmod.item.ModItemGroup;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
     public static final Block DOUGH_BLOCK = registerBlock("dough_block",
             new Block(FabricBlockSettings.of(Material.ORGANIC_PRODUCT).hardness(0.5f).sounds(BlockSoundGroup.HONEY).resistance(0.5f).strength(1f).requiresTool()), ModItemGroup.COOKIES);
+
+    public static final Block COOKIE_ORE = registerBlock("cookie_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(), UniformIntProvider.create(3, 7)), ModItemGroup.COOKIES);
+
+    public static final Block DEEPSLATE_COOKIE_ORE = registerBlock("deepslate_cookie_ore.json",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(), UniformIntProvider.create(3, 7)), ModItemGroup.COOKIES);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
