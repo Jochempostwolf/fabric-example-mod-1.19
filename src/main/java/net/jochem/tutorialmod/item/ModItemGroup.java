9fbe68645a46df747ac130c21d403ecd01259ccd
Jochem <jochempostwolf@gmail.com>
2022-09-20 12:38:31 +0200

    Added itemgroup

--- src/main/java/net/jochem/tutorialmod/item/ModItemGroup.java
+++ src/main/java/net/jochem/tutorialmod/item/ModItemGroup.java
@@ -1,8 +1,12 @@
 package net.jochem.tutorialmod.item;
 
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.jochem.tutorialmod.TutorialMod;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static final ItemGroup COOKIES = FabricItemGroupBuilder.build()
+    public static final ItemGroup COOKIES = FabricItemGroupBuilder.build(
+            new Identifier(TutorialMod.MOD_ID, "cookies"), () -> new ItemStack(ModItems.COOKIE_DOUGH));
 }
--- src/main/java/net/jochem/tutorialmod/item/ModItems.java
+++ src/main/java/net/jochem/tutorialmod/item/ModItems.java
@@ -3,14 +3,13 @@ package net.jochem.tutorialmod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.jochem.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item COOKIE_DOUGH = registerItem("cookie_dough",
-            new Item(new FabricItemSettings().group(ItemGroup.FOOD)));
+            new Item(new FabricItemSettings().group(ModItemGroup.COOKIES)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
