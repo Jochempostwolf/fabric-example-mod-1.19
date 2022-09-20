85a3cf061eaf71c81aab39af533c7abbf1acd0a3
Jochem <jochempostwolf@gmail.com>
2022-09-20 12:17:40 +0200

    Saved changes

--- src/main/java/net/jochem/tutorialmod/TutorialMod.java
+++ src/main/java/net/jochem/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.jochem.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.jochem.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 
 	}
 }
