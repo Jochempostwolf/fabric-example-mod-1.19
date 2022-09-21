fc069f50383dc115e402e560f676aabaa52a55f2
Jochem <jochempostwolf@gmail.com>
2022-09-21 21:50:02 +0200

    Added more cookies and recipes

--- src/main/java/net/jochem/tutorialmod/item/ModItems.java
+++ src/main/java/net/jochem/tutorialmod/item/ModItems.java
@@ -11,9 +11,14 @@ public class ModItems {
 
     public static final Item COOKIE_DOUGH = registerItem("cookie_dough",
             new Item(new FabricItemSettings().group(ModItemGroup.COOKIES)));
+    public static final Item GRANDMAS_DOUGH =registerItem("grandmas_dough",
+            new Item(new FabricItemSettings().group(ModItemGroup.COOKIES)));
     public static final Item BAKED_COOKIE = registerItem("baked_cookie",
             new Item(new FabricItemSettings().group(ModItemGroup.COOKIES)
                     .food(new FoodComponent.Builder().snack().hunger(5).saturationModifier(5f).build())));
+    public static final Item GRANDMAS_COOKIE = registerItem("grandmas_cookie",
+            new Item(new FabricItemSettings().group(ModItemGroup.COOKIES)
+                    .food(new FoodComponent.Builder().snack().hunger(6).saturationModifier(7f).build())));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
